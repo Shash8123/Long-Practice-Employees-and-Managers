863a3e0ada2684fdbc0fdda08b185edc45585d05
Shashank <shashank51199@gmail.com>
2023-05-23 15:07:45 +0530

    final commit

---
+++ employee.js
@@ -0,0 +1,32 @@
+class Employee{
+    constructor(name,salary,title,manager=null){
+        this.name = name;
+        this.salary = salary;
+        this.title = title;
+        this.manager = manager
+
+        if(manager){
+            manager.addEmployee(this)
+        }
+    }
+
+    calculateBonus(multiplier){
+        return this.salary*multiplier
+    }
+  
+}
+
+
+
+
+module.exports = Employee 
+
+
+
+// const leo = new Employee('Leonardo', 90000, 'Ninja');
+// console.log(leo)
+// const raph = new Employee('Raphael', 90000, 'Ninja');
+// const donny = new Employee('Donatello', 85000, 'Grasshopper');
+
+// console.log(raph.calculateBonus(0.25)); // => 22500
+// console.log(donny.calculateBonus(0.15)); // => 12750
